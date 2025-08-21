10a2ba4fcf522110fd3843a8895091543db372f1
Aurélien Malisart <aurelien@phonoid.com>
2025-08-21 17:27:04 +0200

    wez and helix configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,14 @@
 local wezterm = require 'wezterm'
 local config  = wezterm.config_builder()
 
-config.font_size    = 14
-config.color_scheme = 'Abernathy'
+COLOR_SCHEME     = 'Tokyo Night'
+SSH_COLOR_SCHEME = 'Red Alert'
+
+config.font_size    = 13
+config.color_scheme = COLOR_SCHEME
 
 -- brew install font-meslo-lg-nerd-font
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font('MesloLGS Nerd Font Mono')
 
 config.send_composed_key_when_left_alt_is_pressed  = true
 config.send_composed_key_when_right_alt_is_pressed = false
@@ -64,10 +67,25 @@ config.keys = {
 
 wezterm.on('format-window-title',
   function(tab, pane)
-    local home = os.getenv("HOME")
-    local cwd  = tostring(pane.current_working_dir.file_path):gsub(home .. "/", "")
+    local home = os.getenv('HOME')
+    local cwd  = tostring(pane.current_working_dir.file_path):gsub(home .. '/', '')
 
     return "WezTerm — " .. tostring(cwd)
+  end
+)
+
+wezterm.on('update-status',
+  function(window, pane)
+    local fg_process_name = pane:get_foreground_process_name()
+    local overrides       = window:get_config_overrides() or {}
+
+    if fg_process_name == '/usr/bin/ssh' then
+      overrides.color_scheme =  SSH_COLOR_SCHEME
+    else
+      overrides.color_scheme = COLOR_SCHEME
+    end
+
+    window:set_config_overrides(overrides)
   end
 )
 
