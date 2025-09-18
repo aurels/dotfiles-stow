7e1ee24c709a90e0a200adb296a06f1d351b3700
Aurélien Malisart <aurelien@phonoid.com>
2025-09-18 12:24:38 +0200

    config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,10 @@ export LANG=en_US.UTF-8
 export EDITOR="hx"
 export GOPATH="/Users/aurels/Dev/golang"
 
-set -Ux fish_user_paths /opt/homebrew/bin $fish_user_paths
+# Settings
+
+# set -Ux fish_user_paths /opt/homebrew/bin $fish_user_paths
 set -U fish_greeting ""
-
-## rbenv
-
-status --is-interactive; and rbenv init - fish | source
 
 ## Aliases
 
@@ -19,6 +17,8 @@ alias sublime=subl
 alias gitdiff="git icdiff"
 alias dc="docker compose"
 alias wezterm=/Applications/WezTerm.app/Contents/MacOS/wezterm
+alias utm="/Applications/UTM.app/Contents/MacOS/utm"
+alias utmctl="/Applications/UTM.app/Contents/MacOS/utmctl"
 
 ## SSH
 
@@ -27,13 +27,10 @@ alias wezterm=/Applications/WezTerm.app/Contents/MacOS/wezterm
 # ssh-add /Users/aurels/.ssh/reprtoir.pem >&/dev/null
 # ssh-add /Users/aurels/.ssh/rvd-server-pilot.pem >&/dev/null
 
-## cd / zoxide
+## zoxide/eza
 
 zoxide init fish | source
 alias cd=z
-
-## ls / eza
-
 alias ls="eza -ali"
 
 ## Mise
--- launch-reprtoir.sh
+++ launch-reprtoir.sh
@@ -16,22 +16,30 @@ run_command() {
     echo -e "$command_to_run\n" | wezterm cli send-text --no-paste --pane-id "$pane_id"
 }
 
+helix_pane=$(wezterm cli spawn --cwd "$PROJECT_DIR")
+lazygit_pane=$(wezterm cli spawn --cwd "$PROJECT_DIR")
 foreman_pane=$(wezterm cli spawn --cwd "$PROJECT_DIR")
 log_pane=$(wezterm cli spawn --cwd "$PROJECT_DIR")
-sidekiq_pane=$(wezterm cli split-pane --right --pane-id "$log_pane")
+sidekiq_pane=$(wezterm cli spawn --cwd "$PROJECT_DIR")
+# sidekiq_pane=$(wezterm cli split-pane --right --pane-id "$log_pane")
 console_pane=$(wezterm cli spawn --cwd "$PROJECT_DIR")
 git_pane=$(wezterm cli spawn --cwd "$PROJECT_DIR")
 
+run_command "$helix_pane" 'hx'
+run_command "$lazygit_pane" 'lazygit'
 run_command "$foreman_pane" 'foreman start -f Procfile.dev'
 run_command "$log_pane" 'tail -f log/development.log'
 run_command "$sidekiq_pane" 'touch tmp/restart.txt && bundle exec sidekiq -c 1'
 run_command "$console_pane" 'bundle exec rails console'
 run_command "$git_pane" 'git status'
 
-wezterm cli set-tab-title --pane-id "$foreman_pane" "foreman + docker"
-wezterm cli set-tab-title --pane-id "$log_pane" "logs + sidekiq"
+wezterm cli set-tab-title --pane-id "$helix_pane" "helix"
+wezterm cli set-tab-title --pane-id "$lazygit_pane" "lazygit"
+wezterm cli set-tab-title --pane-id "$foreman_pane" "foreman"
+wezterm cli set-tab-title --pane-id "$log_pane" "rails logs"
+wezterm cli set-tab-title --pane-id "$sidekiq_pane" "sidekiq"
 wezterm cli set-tab-title --pane-id "$console_pane" "rails console"
-wezterm cli set-tab-title --pane-id "$git_pane" "git"
+# wezterm cli set-tab-title --pane-id "$git_pane" "git"
 
 echo "Reprtoir launched"
 
